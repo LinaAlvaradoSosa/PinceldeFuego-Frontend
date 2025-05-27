a8c7562ea4c4fd4b7be13153f27472f9be256271
linaA <hellolinita@gmail.com>
2025-05-26 20:42:50 -0500

    Nav,Footer y home con responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { ArtistaComponent } from './pages/artista/artista.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 export const routes: Routes = [
     {path: "" , component: HomeComponent },
-    {path: "galeria", component: GaleriaComponent }
+    {path: "galeria", component: GaleriaComponent},
+    {path: "artista", component:ArtistaComponent},
+    {path: "contacto", component: ContactoComponent}
 ];
